4358d7d39ac384a108b86fa5d7b49f2b9c169a97
Demon154 <chriswhitelol77@gmail.com>
2022-08-26 21:39:40 +0500

    add loop.js

---
+++ loop.js
@@ -0,0 +1,30 @@
+// loops //
+ // types of loops //
+ // for//
+ // for of //
+ // for in //
+ // while //
+ // do while //
+ 
+ // for loop //
+ // if we want to write a string 5 times we can do it with a for loop //
+ var names = ["john","daniel","michael","jimmy","waleed"]
+ for (var i = 1; i <= 4; i++){
+    console.log(JSON.stringify(names))
+ }
+ // this will give the output ["john","daniel","michael","jimmy","waleed"] 5 times
+ for (var i = 1; i <= 5; i++){
+    console.log(names)
+ }
+ // this will give us output in array form
+ for (var i = 0; i < names.length; i++){
+    console.log(names[i])
+ }
+ // this will give us output
+/*
+john
+daniel
+michael
+jimmy
+waleed
+*/
